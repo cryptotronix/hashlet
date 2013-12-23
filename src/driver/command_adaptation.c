af2bce3785c81d0ece3823b0cccaef029058cbac
Josh Datko <jbdatko@gmail.com>
2013-12-23 16:48:18 +0000

    Fixed formatting to the correct style.

--- src/driver/command_adaptation.c
+++ src/driver/command_adaptation.c
@@ -33,16 +33,16 @@
 bool process_command (int fd, struct Command_ATSHA204 *c,
                       uint8_t* rec_buf, unsigned int recv_len)
 {
-    unsigned int c_len = 0;
-    uint8_t *serialized;
+  unsigned int c_len = 0;
+  uint8_t *serialized;
 
-    assert (NULL != c);
-    assert (NULL != rec_buf);
+  assert (NULL != c);
+  assert (NULL != rec_buf);
 
-    c_len = serialize_command (c, &serialized);
+  c_len = serialize_command (c, &serialized);
 
-    return send_and_receive (fd, serialized, c_len, rec_buf, recv_len,
-                             &c->exec_time);
+  return send_and_receive (fd, serialized, c_len, rec_buf, recv_len,
+                           &c->exec_time);
 
 }
 
@@ -50,137 +50,137 @@ int send_and_receive (int fd, uint8_t *send_buf, unsigned int send_buf_len,
                       uint8_t *recv_buf, unsigned int recv_buf_len,
                       struct timespec *wait_time)
 {
-    struct timespec tim_rem;
-    int result = 0;
+  struct timespec tim_rem;
+  int result = 0;
 
-    assert (NULL != send_buf);
-    assert (NULL != recv_buf);
-    assert (NULL != wait_time);
+  assert (NULL != send_buf);
+  assert (NULL != recv_buf);
+  assert (NULL != wait_time);
 
-    print_hex_string ("Sending", send_buf, send_buf_len);
+  print_hex_string ("Sending", send_buf, send_buf_len);
 
-    result = i2c_write (fd,send_buf,send_buf_len);
+  result = i2c_write (fd,send_buf,send_buf_len);
 
-    if (result > 1)
+  if (result > 1)
     {
 
-        nanosleep (wait_time , &tim_rem);
+      nanosleep (wait_time , &tim_rem);
 
-        read_and_validate (fd, recv_buf, recv_buf_len);
+      read_and_validate (fd, recv_buf, recv_buf_len);
 
     }
-    else
+  else
     {
-        perror ("Send failed\n");
-        exit (1);
+      perror ("Send failed\n");
+      exit (1);
     }
 
-    return result;
+  return result;
 }
 
 unsigned int serialize_command (struct Command_ATSHA204 *c, uint8_t **serialized)
 {
-    unsigned int total_len = 0;
-    unsigned int crc_len = 0;
-    unsigned int crc_offset = 0;
-    uint8_t *data;
-    uint16_t *crc;
+  unsigned int total_len = 0;
+  unsigned int crc_len = 0;
+  unsigned int crc_offset = 0;
+  uint8_t *data;
+  uint16_t *crc;
 
 
-    assert (NULL != c);
-    assert (NULL != serialized);
+  assert (NULL != c);
+  assert (NULL != serialized);
 
-    total_len = sizeof (c->command) + sizeof (c->count) +sizeof (c->opcode) +
-        sizeof (c->param1) + sizeof (c->param2) + c->data_len + sizeof (c->checksum);
+  total_len = sizeof (c->command) + sizeof (c->count) +sizeof (c->opcode) +
+    sizeof (c->param1) + sizeof (c->param2) + c->data_len + sizeof (c->checksum);
 
-    crc_len = total_len - sizeof (c->command) - sizeof (c->checksum);
+  crc_len = total_len - sizeof (c->command) - sizeof (c->checksum);
 
-    crc_offset = total_len - sizeof (c->checksum);
+  crc_offset = total_len - sizeof (c->checksum);
 
-    c->count = total_len - sizeof (c->command);
+  c->count = total_len - sizeof (c->command);
 
-    data = (uint8_t*)malloc (total_len);
+  data = (uint8_t*)malloc (total_len);
 
-    assert (NULL != data);
+  assert (NULL != data);
 
-    print_command (c);
+  print_command (c);
 
-    CTX_LOG (DEBUG,
-             "Total len: %d, count: %d, CRC_LEN: %d, CRC_OFFSET: %d\n",
-             total_len, c->count, crc_len, crc_offset);
+  CTX_LOG (DEBUG,
+           "Total len: %d, count: %d, CRC_LEN: %d, CRC_OFFSET: %d\n",
+           total_len, c->count, crc_len, crc_offset);
 
-    /* copy over the command */
-    data[0] = c->command;
-    data[1] = c->count;
-    data[2] = c->opcode;
-    data[3] = c->param1;
-    data[4] = c->param2[0];
-    data[5] = c->param2[1];
-    if (c->data_len > 0)
-        memcpy (&data[6], c->data, c->data_len);
+  /* copy over the command */
+  data[0] = c->command;
+  data[1] = c->count;
+  data[2] = c->opcode;
+  data[3] = c->param1;
+  data[4] = c->param2[0];
+  data[5] = c->param2[1];
+  if (c->data_len > 0)
+    memcpy (&data[6], c->data, c->data_len);
 
-    crc = (uint16_t *)&data[crc_offset];
-    *crc = calculate_crc16 (&data[1], crc_len);
+  crc = (uint16_t *)&data[crc_offset];
+  *crc = calculate_crc16 (&data[1], crc_len);
 
-    *serialized = data;
+  *serialized = data;
 
-    return total_len;
+  return total_len;
 
 }
 
 bool read_and_validate (int fd, uint8_t *buf, unsigned int len)
 {
 
-    uint8_t* tmp = NULL;
-    const int PAYLOAD_LEN_SIZE = 1;
-    const int CRC_SIZE = 2;
-    bool result = false;
-    unsigned int recv_buf_len = 0;
-    bool crc_valid;
-    unsigned int crc_offset;
-    int read_bytes;
+  uint8_t* tmp = NULL;
+  const int PAYLOAD_LEN_SIZE = 1;
+  const int CRC_SIZE = 2;
+  bool result = false;
+  unsigned int recv_buf_len = 0;
+  bool crc_valid;
+  unsigned int crc_offset;
+  int read_bytes;
 
-    assert (NULL != buf);
+  assert (NULL != buf);
 
-    recv_buf_len = len + PAYLOAD_LEN_SIZE + CRC_SIZE;
+  recv_buf_len = len + PAYLOAD_LEN_SIZE + CRC_SIZE;
 
-    crc_offset = recv_buf_len - 2;
+  crc_offset = recv_buf_len - 2;
 
-    /* The buffer that comes back has a length byte at the front and a
-     * two byte crc at the end. */
-    tmp = malloc_wipe (recv_buf_len);
+  /* The buffer that comes back has a length byte at the front and a
+   * two byte crc at the end. */
+  tmp = malloc_wipe (recv_buf_len);
 
  TRY_AGAIN:
 
-    read_bytes = i2c_read (fd, tmp, recv_buf_len);
+  read_bytes = i2c_read (fd, tmp, recv_buf_len);
 
-    if (read_bytes == recv_buf_len)
+  if (read_bytes == recv_buf_len)
     {
-        print_hex_string ("Received RSP", tmp, recv_buf_len);
+      print_hex_string ("Received RSP", tmp, recv_buf_len);
 
-        crc_valid = is_crc_16_valid (tmp, tmp[0] - CRC_16_LEN, tmp + crc_offset);
+      crc_valid = is_crc_16_valid (tmp, tmp[0] - CRC_16_LEN, tmp + crc_offset);
 
-        if (true == crc_valid)
+      if (true == crc_valid)
         {
-            wipe (buf, len);
-            memcpy (buf, &tmp[1], len);
-            free_wipe (tmp, recv_buf_len);
+          wipe (buf, len);
+          memcpy (buf, &tmp[1], len);
+          free_wipe (tmp, recv_buf_len);
 
-            result = true;
+          result = true;
 
         }
-        else
+      else
         {
-            perror ("CRC FAIL!\n");
+          perror ("CRC FAIL!\n");
         }
     }
-    else
+  else
     {
-        CTX_LOG (DEBUG,"Read failed, retrying");
-        goto TRY_AGAIN;
+      CTX_LOG (DEBUG,"Read failed, retrying");
+      goto TRY_AGAIN;
 
     }
 
 
-    return result;
+  return result;
 }

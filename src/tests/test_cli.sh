2ca82ea1039c08cbc89339451fd21e60c2efa629
Josh Datko <jbdatko@gmail.com>
2013-12-29 20:34:04 +0000

    Adds licenses to files.

--- src/tests/test_cli.sh
+++ src/tests/test_cli.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Copyright (C) 2013 Cryptotronix, LLC.
+
+# This file is part of Hashlet.
+
+# Hashlet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# Hashlet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Hashlet.  If not, see <http://www.gnu.org/licenses/>.
+
 
 BUS=/dev/i2c-1
 EXE=./hashlet

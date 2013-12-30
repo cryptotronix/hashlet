c909219db62ef7156f631fee0fc6beae3452bb37
Josh Datko <jbdatko@gmail.com>
2013-12-30 15:27:44 +0000

    Moves scripts into autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+# Copyright (C) 2013 Cryptotronix, LLC.
 
+# This file is part of Hashlet.
+
+# Hashlet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# Hashlet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Hashlet.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ ! -d "m4" ]; then
+    mkdir m4
+fi
 autoreconf --force --install
+./configure
+make
